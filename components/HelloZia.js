209d278107d157c88b77cd68531dc945c17b84f8
zia <zedlinkus@gmail.com>
2022-03-24 10:36:59 +0100

    setting up nextjs to work properly with tailwind

---
+++ components/HelloZia.js
@@ -0,0 +1,9 @@
+export default function HelloZia() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <span className="text-3xl inline font-bold hover:bg-sky-700 p-8 cursor-pointer">
+        Hello Zia
+      </span>
+    </div>
+  );
+}
